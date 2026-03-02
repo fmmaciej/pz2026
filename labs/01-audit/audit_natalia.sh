ee25bb4baa2eb64285c1123ae46736befd430995
Jakub <jakub.gradys@gmail.com>
2026-03-02 20:16:41 +0100

    labs: zadanie z dnia 2026-03-02 - Refaktoryzacja kodu

--- labs/01-audit/audit_natalia.sh
+++ labs/01-audit/audit_natalia.sh
@@ -3,51 +3,69 @@
 QUICK=false
 PATH_AUDIT_DIR="./audits"
 
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        --quick)
-            QUICK=true
-            shift
-            ;;
-        --out)
-            PATH_AUDIT_DIR="$2"
-            shift 2
-            ;;
-    esac
-done
+parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --quick)
+                QUICK=true
+                shift
+                ;;
+            --out)
+                PATH_AUDIT_DIR="$2"
+                shift 2
+                ;;
+        esac
+    done
+}
 
-mkdir -p $PATH_AUDIT_DIR
-path=$PATH_AUDIT_DIR/audit_$(hostname)_$(date +%Y)-$(date +%m)-$(date +%d)_$(date +%H)$(date +%M)$(date +%S).txt
-touch $path
+create_file() {
+    mkdir -p $PATH_AUDIT_DIR
+    path=$PATH_AUDIT_DIR/audit_$(hostname)_$(get_date).txt
 
-echo "METADANE AUDYTU" >> $path
-echo "Użytkownik: $(whoami)" >> $path
-echo "Data: $(date)" >> $path
-echo "Nazwa hosta: $(hostname)" >> $path
-if [[ -n "$SSH_CLIENT" ]]; then
-    echo "Połączenie: SSH" >> $path
-    echo "Adres IP: $(echo $SSH_CLIENT | awk '{print $1}')" >> $path
-else
-    echo "Połączenie: LOCAL" >> $path
-fi
+    extract_data_to_file
+}
 
-echo "" >> $path
-echo "INFORMACJE O SYSTEMIE" >> $path
-echo "Wersja systemu: $(cat /etc/os-release)" >> $path
-echo "Kernel i architektura: $(uname -a)" >> $path
-echo "Uptime: $(uptime -p)" >> $path
-echo "Obciążenie systemu: $(uptime)" >> $path
+get_date() {
+    echo $(date +%Y)-$(date +%m)-$(date +%d)_$(date +%H)$(date +%M)$(date +%S)
+}
 
-echo "" >> $path
-echo "ZASOBY" >> $path
-echo "Użycie dysku:" >> $path
-df -h >> $path
-echo "Pamięć:" >> $path
-free -h >> $path
+extract_data_to_file() {
+    {
+        echo "METADANE AUDYTU" 
+        echo "Użytkownik: $(whoami)" 
+        echo "Data: $(date)" 
+        echo "Nazwa hosta: $(hostname)" 
+        if [[ -n "$SSH_CLIENT" ]]; then
+            echo "Połączenie: SSH" 
+            echo "Adres IP: $(echo $SSH_CLIENT | awk '{print $1}')" 
+        else
+            echo "Połączenie: LOCAL" 
+        fi
 
-if [[ $QUICK == false ]]; then
-    echo "Top 5 największych katalogów w \$HOME:" >> $path
-    du -sh "$HOME"/* 2>/dev/null | sort -h | tail -n 5 >> $path
-fi
+        echo "" 
+        echo "INFORMACJE O SYSTEMIE" 
+        echo "Wersja systemu: $(cat /etc/os-release)" 
+        echo "Kernel i architektura: $(uname -a)" 
+        echo "Uptime: $(uptime -p)" 
+        echo "Obciążenie systemu: $(uptime)" 
 
-exit
+        echo "" 
+        echo "ZASOBY" 
+        echo "Użycie dysku:" 
+        df -h 
+        echo "Pamięć:" 
+        free -h 
+
+        if [[ $QUICK == false ]]; then
+            echo "Top 5 największych katalogów w \$HOME:" 
+            du -sh "$HOME"/* 2>/dev/null | sort -h | tail -n 5 
+        fi
+    } > $path;
+}
+
+main() {
+    parse_args
+    create_file    
+}
+
+main
