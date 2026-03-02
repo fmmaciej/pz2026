82473da4f66aa65e155550116be3b7bca8fb7852
Natalia <kuszewska.n@gmail.com>
2026-03-02 20:48:24 +0100

    Commit z dnia 2026-03-02
    - journal: dodanie notatek
    - labs: refaktoryzacja skryptu audit_jakub.sh

--- labs/01-audit/audit_jakub.sh
+++ labs/01-audit/audit_jakub.sh
@@ -1,28 +1,49 @@
 #!/usr/bin/env bash
 
 NOT_QUICK=true
-PATH_AUDIT_DIR="./audits"
+
+CONFIG_PATH="$HOME/.config/audit_jakub"
+CONFIG_NAME="audit.conf"
+FULL_CONFIG_PATH="$CONFIG_PATH/$CONFIG_NAME"
 
 parse_args() {
     while [[ $# -gt 0 ]]; do
         case "$1" in 
             -o | --out)
-                make_dir "$2"
-                shift 2
+                [[ -z "$2"]] && echo "Brak podanej ścieżki dla: $1"; exit 1
+
+                PATH_AUDIT_DIR="$2"
+
+                case "$3" in
+                    -s | --setdefault)
+                        sed -i "s|^PATH_AUDIT_DIR=.*|PATH_AUDIT_DIR=\"$2\"|" "$FULL_CONFIG_PATH"
+                        shift 3
+                        ;;
+                    *)
+                        shift 2
+                esac
                 ;;
             -q | --quick)
                 NOT_QUICK=false
                 shift
                 ;;
             *)
-            echo "Unknown arg: $1"
+            echo "Nieznany argument: $1"
+            exit 1
+            ;;
         esac
     done
 }
 
-make_dir() {
-    PATH_AUDIT_DIR="${1:-$PATH_AUDIT_DIR}"
-    [[ -d "$PATH_AUDIT_DIR" ]] || mkdir -p "$PATH_AUDIT_DIR"
+load_config() {
+    if [[ ! -f "$FULL_CONFIG_PATH" ]]; then
+        mkdir -p "$CONFIG_PATH"
+        echo "PATH_AUDIT_DIR=\"./audits\"" > "$FULL_CONFIG_PATH"
+
+        source "$FULL_CONFIG_PATH"
+    else
+        source "$FULL_CONFIG_PATH"
+    fi
 }
 
 create_file() {
@@ -65,8 +86,9 @@ create_file() {
 }
 
 main() {
+    load_config
     parse_args "$@"
-    make_dir
+    [[ -d "$PATH_AUDIT_DIR" ]] || mkdir -p "$PATH_AUDIT_DIR"
     create_file
 }
 
